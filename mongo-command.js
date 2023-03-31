9cc2283ef0df575475907e28050d1a49f8b6e3ea
Poonam <poonam.chauhan229@gmail.com>
2023-03-31 11:14:44 +0530

    first commit

---
+++ mongo-command.js
@@ -0,0 +1,20 @@
+show dbs
+>admin
+>config
+>local
+
+
+> use local
+switched to db local
+> db
+local
+> use guvi-db
+switched to db guvi-db
+> db
+guvi-db
+
+show collections
+
+How to create collections?
+
+
